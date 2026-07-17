4d88c5b3907df0d083612c23de0263d7349786f6
Sebastian Rosas <rosas.gomez.axelsebastian010606@gmail.com>
2026-07-17 17:08:25 -0600

    Actualización debido a problema de rutas

--- src/data_loader.py
+++ src/data_loader.py
@@ -1,6 +1,10 @@
 import wfdb
 import numpy as np
 import matplotlib.pyplot as plt
+from pathlib import Path
+
+wfdb.dl_database('mitdb', dl_dir='../src/data/mitdb', records=['100', '106', '200'])
+PROJECT_ROOT = Path(__file__).parent.parent
 
 def analisis(path):
     record = wfdb.rdrecord(path)
@@ -21,5 +25,17 @@ def graficar(record, annotation):
     plt.legend()
     plt.show()
 
+def graficarRange(record, annotation, inicio, fin):
+    seg = record.p_signal[inicio:fin, 0]
+    time = np.arange(inicio, fin) /record.fs
+    plt.plot(time, seg)
+    picos = annotation.sample[annotation.sample < fin & annotation.sample > inicio]
+    timehigh = picos / record.fs
+    plt.scatter(timehigh, seg[picos], color='red', label='R-peaks')
+    plt.title('ECG Signal with R-peaks')
+    plt.xlabel('Time (s)')
+    plt.ylabel('Amplitude')
+    plt.legend()
+    plt.show()
 
-record, annotation = analisis('data/mitdb/100')
+record, annotation = analisis('../src/data/mitdb/100')
